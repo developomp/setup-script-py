330f655932b2a41a61aa134b536b6bd8e47c06c7
developomp <developomp@gmail.com>
2022-01-27 21:52:27 +0900

    added more logging

--- src/initialize.py
+++ src/initialize.py
@@ -8,17 +8,18 @@ def initialize():
     Initialize before running any code.
     """
 
-    # Install flatpak
+    log.log("Initializing flatpak")
     if os.system("sudo pacman -S --noconfirm --needed flatpak &> /dev/null"):
         log.error("Failed to install flatpak form archlinux package repository")
         exit(1)
 
-    # Install pip
+    log.log("Initializing pip")
     if os.system("sudo pacman -S --noconfirm --needed python-pip &> /dev/null"):
         log.error("Failed to install pip from archlinux package repository")
         exit(1)
 
-    # install [pytermgui](https://github.com/bczsalba/pytermgui)
+    # https://github.com/bczsalba/pytermgui
+    log.log("Initializing pytermgui")
     if os.system("pip install pytermgui &> /dev/null"):
         log.error("Failed to install pytermgui from pip")
         exit(1)
