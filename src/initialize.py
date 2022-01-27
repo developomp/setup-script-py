bfecfc2f5860aa01b6a9e28168d22e37be9be491
developomp <developomp@gmail.com>
2022-01-27 21:30:34 +0900

    added pip install to initialization

--- src/initialize.py
+++ src/initialize.py
@@ -6,10 +6,14 @@ from . import log
 def initialize():
     """
     - install [pytermgui](https://github.com/bczsalba/pytermgui)
-    - install pip
     """
 
     # Install flatpak
-    if os.system("sudo pacman -S --noconfirm --needed flatpak"):
+    if os.system("sudo pacman -S --noconfirm --needed flatpak &> /dev/null"):
         log.error("Failed to install flatpak")
+        exit(1)
+
+    # Install pip
+    if os.system("sudo pacman -S --noconfirm --needed python-pip &> /dev/null"):
+        log.error("Failed to install pip")
         exit(1)
