78ed312512e4f125fccea2a5c34760e578c721bf
developomp <developomp@gmail.com>
2022-01-27 21:36:23 +0900

    improved initialization added python dependency installation to initialization

--- src/initialize.py
+++ src/initialize.py
@@ -5,15 +5,20 @@ from . import log
 
 def initialize():
     """
-    - install [pytermgui](https://github.com/bczsalba/pytermgui)
+    Initialize before running any code.
     """
 
     # Install flatpak
     if os.system("sudo pacman -S --noconfirm --needed flatpak &> /dev/null"):
-        log.error("Failed to install flatpak")
+        log.error("Failed to install flatpak form archlinux package repository")
         exit(1)
 
     # Install pip
     if os.system("sudo pacman -S --noconfirm --needed python-pip &> /dev/null"):
-        log.error("Failed to install pip")
+        log.error("Failed to install pip from archlinux package repository")
+        exit(1)
+
+    # install [pytermgui](https://github.com/bczsalba/pytermgui)
+    if os.system("pip install pytermgui &> /dev/null"):
+        log.error("Failed to install pytermgui from pip")
         exit(1)
