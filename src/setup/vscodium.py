8210ac95656650cdab3f27e5353d537385aa121a
developomp <developomp@gmail.com>
2022-01-30 18:11:25 +0900

    moved extension list outside of the function

--- src/setup/vscodium.py
+++ src/setup/vscodium.py
@@ -6,51 +6,52 @@ from os import system
 name = "Vscodium"
 
 
+EXTENSIONS = (
+    "aaron-bond.better-comments",
+    "bierner.jsdoc-markdown-highlighting",
+    "dbaeumer.vscode-eslint",
+    "denoland.vscode-deno",
+    "DigitalBrainstem.javascript-ejs-support",
+    "dsznajder.es7-react-js-snippets",
+    "eamodio.gitlens",
+    "earshinov.sort-lines-by-selection",
+    "EditorConfig.EditorConfig",
+    "esbenp.prettier-vscode",
+    "foxundermoon.shell-format",
+    "geequlim.godot-tools",
+    "golang.go",
+    "jeff-tian.markdown-katex",
+    "jock.svg",
+    "matklad.rust-analyzer",
+    "mhutchie.git-graph",
+    "ms-python.python",
+    "ms-toolsai.jupyter",
+    "ms-toolsai.jupyter-keymap",
+    "ms-toolsai.jupyter-renderers",
+    "ms-vscode.hexeditor",
+    "naumovs.color-highlight",
+    "nico-castell.linux-desktop-file",
+    "PKief.material-icon-theme",
+    "qwtel.sqlite-viewer",
+    "Razoric.gdscript-toolkit-formatter",
+    "redwan-hossain.auto-rename-tag-clone",
+    "serayuzgur.crates",
+    "streetsidesoftware.code-spell-checker",
+    "styled-components.vscode-styled-components",
+    "svelte.svelte-vscode",
+    "tamasfe.even-better-toml",
+    "XadillaX.viml",
+    "zhuangtongfa.material-theme",
+)
+
+
 def setup():
     """vscode without microsoft"""
 
     flatpak_install("com.vscodium.codium")
 
     # codium --list-extensions
-    extensions = (
-        "aaron-bond.better-comments",
-        "bierner.jsdoc-markdown-highlighting",
-        "dbaeumer.vscode-eslint",
-        "denoland.vscode-deno",
-        "DigitalBrainstem.javascript-ejs-support",
-        "dsznajder.es7-react-js-snippets",
-        "eamodio.gitlens",
-        "earshinov.sort-lines-by-selection",
-        "EditorConfig.EditorConfig",
-        "esbenp.prettier-vscode",
-        "foxundermoon.shell-format",
-        "geequlim.godot-tools",
-        "golang.go",
-        "jeff-tian.markdown-katex",
-        "jock.svg",
-        "matklad.rust-analyzer",
-        "mhutchie.git-graph",
-        "ms-python.python",
-        "ms-toolsai.jupyter",
-        "ms-toolsai.jupyter-keymap",
-        "ms-toolsai.jupyter-renderers",
-        "ms-vscode.hexeditor",
-        "naumovs.color-highlight",
-        "nico-castell.linux-desktop-file",
-        "PKief.material-icon-theme",
-        "qwtel.sqlite-viewer",
-        "Razoric.gdscript-toolkit-formatter",
-        "redwan-hossain.auto-rename-tag-clone",
-        "serayuzgur.crates",
-        "streetsidesoftware.code-spell-checker",
-        "styled-components.vscode-styled-components",
-        "svelte.svelte-vscode",
-        "tamasfe.even-better-toml",
-        "XadillaX.viml",
-        "zhuangtongfa.material-theme",
-    )
-
-    for extension in extensions:
+    for extension in EXTENSIONS:
         system(f"codium --install-extension {extension}")
 
     # autostart vscodium
