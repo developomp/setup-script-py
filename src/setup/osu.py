9fa51d4b6b3f5861172aabde08c3bcf551794859
developomp <developomp@gmail.com>
2022-02-02 21:51:37 +0900

    removed osu from setup

--- src/setup/osu.py
+++
@@ -1,9 +0,0 @@
-from ..util import flatpak_install
-
-name = "osu!"
-
-
-def setup():
-    """Sucks all the joy out of your life but it's so fucking addictive"""
-
-    flatpak_install("sh.ppy.osu")
