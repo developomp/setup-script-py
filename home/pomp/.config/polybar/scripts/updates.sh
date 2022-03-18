9fcf2c14326c8f259fbad66beee28dec0fb2dd05
developomp <developomp@gmail.com>
2022-03-18 18:25:14 +0900

    removed bspwm and polybar

--- home/pomp/.config/polybar/scripts/updates.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# add flatpak and pamac update counts
-if ! UPDATES=$(($(checkupdates 2>/dev/null | wc -l) + $(echo 'n' | flatpak update 2>/dev/null | tail -n +5 | head -2 | wc -l))); then
-    UPDATES=0
-fi
-
-echo $UPDATES
