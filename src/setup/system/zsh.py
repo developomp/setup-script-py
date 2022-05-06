a1a2ecad8bfd0e01fd990ad9fd851c4894d48e80
developomp <developomp@gmail.com>
2022-05-06 16:46:18 +0900

    updated zsh installation logic - prevent installation skipping - remove existing cloned repo - use absolute path - set system shell in case it's not done during installation

--- src/setup/system/zsh.py
+++ src/setup/system/zsh.py
@@ -1,4 +1,4 @@
-from src.util import paru_install, copy_file
+from src.util import paru_install, copy_file, remove_directory
 from src.constants import content_dir, home_dir
 from src import log
 
@@ -13,24 +13,29 @@ def setup():
 
     paru_install("zsh")
 
-    if isdir(f"{home_dir}/.oh-my-zsh"):
-        log.log("zsh already configured. Skipping.")
-        return
-
-    # install oh my zsh
+    log.log("Installing Oh My Zsh")
     system(
         'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
     )
 
-    # install powerlevel10k theme
+    log.log("Installing powerlevel10k theme")
+    p10k_path = f"{home_dir}/.oh-my-zsh/custom/themes/powerlevel10k"
+    remove_directory(p10k_path)
     system(
-        "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+        f"git clone --depth=1 https://github.com/romkatv/powerlevel10k.git {p10k_path}"
     )
 
-    # install syntax highlighter
+    log.log("Installing zsh syntax highlighter")
+    zsh_syntax_highlighting_path = (
+        f"{home_dir}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+    )
+    remove_directory(zsh_syntax_highlighting_path)
     system(
-        "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+        f"git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git {zsh_syntax_highlighting_path}"
     )
 
     # apply zshrc configuration
-    copy_file(f"{content_dir}{home_dir}/.zshrc", "~/.zshrc")
+    copy_file(f"{content_dir}{home_dir}/.zshrc", f"{home_dir}/.zshrc")
+
+    # set the default terminal to zsh
+    system("chsh -s /bin/zsh")
