9311c1499f0275d01df44d186f6d5970c8b433d1
developomp <developomp@gmail.com>
2022-06-15 12:37:04 +0900

    Add a post install instruction to Brave browser.

--- src/setup/apps/brave.py
+++ src/setup/apps/brave.py
@@ -1,6 +1,10 @@
 from src.util import flatpak_install
 
 
+# Brave handles restoration itself using its own sync stuff
+post_install = ["enable sync"]
+
+
 def setup():
     """The least worst web browser"""
 
