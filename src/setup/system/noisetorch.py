f876db669548e1e383dce91df9b4a8de759f5827
developomp <developomp@gmail.com>
2022-07-07 10:21:01 +0900

    make noisetorch autostart

--- src/setup/system/noisetorch.py
+++ src/setup/system/noisetorch.py
@@ -1,4 +1,4 @@
-from src.util import paru_install
+from src.util import paru_install, run, copy_file
 
 name = "NoiseTorch"
 
@@ -9,3 +9,11 @@ def setup():
     # temporarily using noisetorch-git over noisetorch-bin due to this:
     # https://github.com/noisetorch/NoiseTorch/issues/274
     paru_install("noisetorch-git")
+
+    # Start NoiseTorch on boot
+    # https://github.com/noisetorch/NoiseTorch/wiki/Start-automatically-with-Systemd
+
+    copy_file("home/.config/systemd/user/noisetorch.service")
+    run("systemctl --user daemon-reload")
+    run("systemctl --user start  noisetorch")
+    run("systemctl --user enable noisetorch")
