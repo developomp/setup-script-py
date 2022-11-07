465a6306dd4a971f460cae99af2bafdd7147444e
developomp <developomp@gmail.com>
2022-11-07 18:21:16 +0900

    add files missed in 65fea2ec

--- src/setup/apps/pomky.py
+++ src/setup/apps/pomky.py
@@ -1,4 +1,4 @@
-from src.util import run, copy_file, command_exists
+from src.util import copy_file, command_exists
 from src.constants import tmp_dir
 
 name = "pomky"
@@ -8,7 +8,9 @@ def setup():
     """conky but rusty"""
 
     if not command_exists("pomky"):
-        run(f"git clone --depth 1 https://github.com/developomp/pomky {tmp_dir}/pomky")
-        run(f"cd {tmp_dir}/pomky && cargo install --path .")
+        system(
+            f"git clone --depth 1 https://github.com/developomp/pomky {tmp_dir}/pomky"
+        )
+        system(f"cd {tmp_dir}/pomky && cargo install --path .")
 
     copy_file("home/.config/autostart/pomky.desktop")
