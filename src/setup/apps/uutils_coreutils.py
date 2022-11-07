d808dd0090a52ae5ec33ff5343ac25281ae7cafe
developomp <developomp@gmail.com>
2022-11-07 18:23:35 +0900

    remove unused setup scripts

--- src/setup/apps/uutils_coreutils.py
+++
@@ -1,9 +0,0 @@
-from src.util import paru_install
-
-name = "uutils-coreutils"
-
-
-def setup():
-    """rusty coreutils"""
-
-    paru_install("uutils-coreutils")
--- src/setup/apps/zoom.py
+++
@@ -1,9 +0,0 @@
-from src.util import flatpak_install
-
-name = "Zoom"
-
-
-def setup():
-    """gay video conference app"""
-
-    flatpak_install("us.zoom.Zoom")
