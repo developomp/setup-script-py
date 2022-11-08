4a97858a0ed09dce25135d673c80dec3e3d42295
developomp <developomp@gmail.com>
2022-11-09 01:59:25 +0900

    move partition data mount point
    from `/media/pomp/data` to `/home/pomp`

--- src/setup/system/fstab.py
+++ src/setup/system/fstab.py
@@ -4,18 +4,16 @@ name = "fstab"
 
 
 def setup():
-    """adds /media/pomp/data drive to fstab"""
+    """adds /home/pomp drive to fstab"""
 
     fstab_path = "/etc/fstab"
 
-    # check if /media/pomp/data exists already
+    # check if /home/pomp exists already
     with open(fstab_path, "rt") as f:
-        if "/media/pomp/data" in f.read():
+        if "/home/pomp" in f.read():
             return
 
-    line_to_write = (
-        "UUID=1cea13a5-ea19-4023-99dd-4bfd062a288c /media/pomp/data ext4 defaults 0 2"
-    )
+    line_to_write = "UUID=1cea13a5-ea19-4023-99dd-4bfd062a288c /home/pomp auto 0 0"
 
     # append a line to the end and ignore output
     # not using python's file interface because I don't wanna deal with permission stuff
