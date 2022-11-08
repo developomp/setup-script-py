3ece6e38cb455831554027cab8e5cabf5aeb9196
developomp <developomp@gmail.com>
2022-11-09 01:21:00 +0900

    notesnook: fix file download path issues

--- src/setup/apps/notesnook.py
+++ src/setup/apps/notesnook.py
@@ -10,7 +10,7 @@ name = "Notesnook"
 def setup():
     """FOSS video editing utility"""
 
-    download_path = f"{Path.home()}/notesnook_linux_x86_64.AppImage"
+    download_path = f"{Path.home()}/Applications/notesnook_linux_x86_64.AppImage"
     download_url = (
         "https://notesnook.com/releases/linux/notesnook_linux_x86_64.AppImage"
     )
@@ -18,6 +18,7 @@ def setup():
     appimagelauncher.setup()
 
     print(f"Downloading AppImage file to {download_path}")
+    Path(download_path).mkdir(parents=True, exist_ok=True)
     open(download_path, "wb").write(
         requests.get(download_url, allow_redirects=True).content
     )
