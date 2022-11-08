fa7b17721337154009be9c7bfa99a84979637c8b
developomp <developomp@gmail.com>
2022-11-08 22:43:11 +0900

    GNOME extension: remove output device chooser

--- src/setup/system/gnome_extensions.py
+++ src/setup/system/gnome_extensions.py
@@ -9,7 +9,6 @@ post_install = ["Restart GNOME shell", "enable GNOME extensions"]
 
 EXTENSIONS = (
     (36, "extension-lockkeys.conf"),  # lock-keys
-    (906, "extension-sound-output-device-chooser.conf"),  # sound-output-device-chooser
     (2741, ""),  # remove-alttab-delay-v2
     (2890, "extension-trayIconsReloaded.conf"),  # tray-icons-reloaded
     (3193, "extension-blur-my-shell.conf"),  # blur-my-shell
@@ -32,7 +31,7 @@ def setup():
         [
             "gnome-shell-extension-installer",
             # not using gnome-shell-extension-pop-shell because it builds from source too anyway
-            # not using gnome-shell-extension-pop-shell-bin because GNOME version isn't compatible
+            # not using gnome-shell-extension-pop-shell-bin because it's unmaintained
             "gnome-shell-extension-pop-shell-git",
         ]
     )
