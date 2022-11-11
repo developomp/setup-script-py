3b6329e656cb6fea60580f37d4a49ae577f3acfe
developomp <developomp@gmail.com>
2022-11-12 01:04:27 +0900

    remove duplicate files

--- src/setup/apps/gnome/document_viewer.py
+++
@@ -1,9 +0,0 @@
-from src.util import flatpak_install
-
-name = "evince"
-
-
-def setup():
-    """Document viewer"""
-
-    flatpak_install("org.gnome.Evince")
--- src/setup/apps/gnome/logs_viewer.py
+++
@@ -1,9 +0,0 @@
-from src.util import flatpak_install
-
-name = "GNOME Logs"
-
-
-def setup():
-    """log viewer GUI"""
-
-    flatpak_install("org.gnome.Logs")
