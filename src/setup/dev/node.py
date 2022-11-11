7e9c568ea9284bfd99583e8cf1d1d0c4365cd251
developomp <developomp@gmail.com>
2022-11-12 00:47:01 +0900

    make use of logging function more consistent

--- src/setup/dev/node.py
+++ src/setup/dev/node.py
@@ -1,7 +1,7 @@
 from src.constants import home_dir
 from src.util import paru_install, command_exists
 from src.setup.system import zsh
-from src import log
+from src.log import log
 
 from os.path import isdir
 from os import system
@@ -26,16 +26,16 @@ def setup():
 
     paru_install("nvm")
 
-    log.log("Installing Node.JS LTS")
+    log("Installing Node.JS LTS")
     system("source /usr/share/nvm/init-nvm.sh; nvm install --lts")
 
     # todo: add "source /usr/share/nvm/init-nvm.sh" to ~/.zshrc
 
-    log.log("Installing npm")
+    log("Installing npm")
     system("npm install --global npm")
 
-    log.log("Installing pnpm")
+    log("Installing pnpm")
     system("npm install --global pnpm")
 
-    log.log("Installing yarn")
+    log("Installing yarn")
     system("npm install --global yarn")
