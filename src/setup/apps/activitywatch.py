b152d947ad9e1076ddcaae13dc86b22def8fe980
developomp <developomp@gmail.com>
2022-11-21 00:09:16 +0900

    remove activitywatch

--- src/setup/apps/activitywatch.py
+++
@@ -1,8 +0,0 @@
-from src.util import paru_install, copy_file
-
-desc = "Local activity monitor"
-
-
-def setup():
-    paru_install("activitywatch-bin")
-    copy_file("home/.config/autostart/activitywatch.desktop")
