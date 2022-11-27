1426c8f06d91a072c5fbb8a20a7c87c1fd0c6837
developomp <developomp@gmail.com>
2022-11-27 19:17:55 +0900

    make notesnook use flatpak instead of AppImage
    - close #118

--- src/setup/apps/notesnook.py
+++ src/setup/apps/notesnook.py
@@ -1,10 +1,7 @@
-from src.util import appimage_install
+from src.util import flatpak_install
 
 desc = "FOSS Note taking utility"
 
 
 def setup():
-    appimage_install(
-        "https://notesnook.com/releases/linux/notesnook_linux_x86_64.AppImage",
-        "notesnook_linux_x86_64",
-    )
+    flatpak_install("com.notesnook.Notesnook")
